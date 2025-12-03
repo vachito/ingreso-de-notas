2fdef849646b8052d9b5fda3c3cb1a47e4a14619
Rigo <vacho19@gmail.com>
2025-12-03 15:05:06 -0600

    componentes nuevos

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,95 @@
+<script lang="ts">
+export const description
+    = "A sidebar that collapses to icons."
+export const iframeHeight = "800px"
+export const containerClass = "w-full h-full"
+</script>
+
+<script setup lang="ts">
+import AppSidebar from "@/components/AppSidebar.vue"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+</script>
+
+<template>
+    <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+            <header
+                class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div class="flex items-center gap-2 px-4">
+                    <SidebarTrigger class="-ml-1" />
+                    <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem class="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator class="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+            </header>
+
+            <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div class="bg-muted/50 aspect-video rounded-xl" />
+                    <div class="bg-muted/50 aspect-video rounded-xl" />
+                    <div class="bg-muted/50 aspect-video rounded-xl" />
+                </div>
+                <div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min p-4">
+                    <Table>
+                        <TableCaption>Lista de componentes internos y externos</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead class="">id</TableHead>
+                                <TableHead>Nombre</TableHead>
+                                <TableHead>descripcion</TableHead>
+                                <TableHead class="text-left">Precio</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell class="font-medium">
+                                    INV001
+                                </TableCell>
+                                <TableCell>Paid</TableCell>
+                                <TableCell>Credit Card</TableCell>
+                                <TableCell class="text-right">
+                                    $250.00
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+        </SidebarInset>
+    </SidebarProvider>
+</template>
